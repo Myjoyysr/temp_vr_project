353274687150b8a24c725083a3582f9c98390ea3
JokkeXD <101457922+JokkeXD@users.noreply.github.com>
2022-12-05 16:34:31 +0200

    Added assets for basic functionalities and camera tilt.
    Added assets for tilting camera with a joystick, teleportation, hand functionality.

---
+++ Assets/TiltVisionWithJoystick.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TiltVisionWithJoystick : MonoBehaviour
+{
+    public XRNode inputSource;
+    private Vector2 inputAxis;
+
+    public float sensitivityX = 0.1F;
+	public float sensitivityY = 0.1F;
+
+	public float minimumX = -90F;
+	public float maximumX = 90F;
+
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+        float rotationX = transform.localEulerAngles.y + inputAxis.x * sensitivityX;
+			
+		rotationY += inputAxis.y * sensitivityY;
+		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		
+    }
+}
