d8b7e63c4a0eaf8ff0dbbfed43d1e418d6ee218a
Myjoyysr <janne.vihriala@gmail.com>
2022-12-10 19:01:38 +0200

    scene6 stuff

---
+++ Assets/Main/Scripts/Scene6/ControlVisionWithController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlVisionWithController : MonoBehaviour
+{
+    private bool onController;
+
+    public GameObject cam;
+    public GameObject controller;
+
+    private Vector3 rotation;
+
+    void Start()
+    {
+        onController = false;
+    }
+
+    void Update()
+    {
+        if(onController == true){
+            rotation = new Vector3(controller.transform.eulerAngles.x, controller.transform.eulerAngles.x, controller.transform.eulerAngles.z);
+            cam.transform.eulerAngles = rotation;
+            cam.transform.position = controller.transform.position;
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("test");
+        if (other.tag == "ControllerCollider"){
+            if (onController == true){
+                onController = false;
+                Debug.Log("Set false");
+            }
+            else{
+                onController = true;
+                Debug.Log("Set true");
+            }
+        }
+    }
+}
